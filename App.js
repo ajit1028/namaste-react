fb378bda03958437125f0c35958c4691005661fb
ajit1028 <ajeets1028@gmail.com>
2024-01-30 13:36:46 +0530

    React Element and Component

--- App.js
+++ App.js
@@ -2,20 +2,21 @@
 import React from "react";
  import ReactDOM from "react-dom/client";
 
-  //React.createElement => Object =>HTMLElement(render)
-  //core or react
-  const heading = React.createElement(
-    "h1",
-    {id: "heading"},
-    "Namaste React 🚀"
+//React Element
+const heading = (<h1 id="heading" className="head">
+    Namaste react using JSX 🚀
+    </h1>
   );
+   
+//React Functional Component
+const HeadingComponent = ()=>(
+  <div id ="container">
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
 
-  console.log(heading);
+);
 
-  //JSX - HTML-like or XML-like syntax
 
-  const jsxHeading = <h1 id="heading ">Namaste react using JSX 🚀</h1>;
-  console.log(jsxHeading);
-
+ 
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(heading);
+  root.render(<HeadingComponent />);
