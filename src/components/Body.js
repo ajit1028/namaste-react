8243e01673895bcd5d9031a49cb29748ce95c688
ajit <ajeets@1028.com>
2024-03-18 20:49:32 +0530

    tailwindcss

--- src/components/Body.js
+++ src/components/Body.js
@@ -46,17 +46,17 @@ const Body = () => {
     <Shimmer/>
     ) : (
       <div className ="body">
-        <div className="filter">
-          <div className = "search">
+        <div className="filter flex">
+          <div className = "search m-4 p-4">
             <input 
             type="text" 
-            className = "search-box" 
+            className = "border border-solid border-black" 
             value={searchText} 
             onChange ={(e) => {
             setSearchText(e.target.value);
             }}
             />
-            <button 
+            <button className = "px-4 py-2 bg-green-100 m-4 rounded-lg"
              onClick={() => {
              console.log(searchText);
 
@@ -71,7 +71,9 @@ const Body = () => {
               Search</button>
 
           </div>
-          <button className = "filter-btn"
+
+          <div className="search m-4 p-4 flex items-center">
+          <button className = "px-4 py-2 bg-gray-100 rounded-lg"
            onClick = {() => {
             
             const filterList = listOfResturants.filter(
@@ -80,8 +82,10 @@ const Body = () => {
                 setListOfResturant(filterList);
             }}
             >Top Rated Resturants</button> 
+          </div>
+         
         </div>
-        <div className="res-container">
+        <div className="flex flex-wrap">
       {filteredResturant.map((restaurant) => (
     <Link key={restaurant.info.id} to={"resturants/" + restaurant.info.id}>
       <ResturantCard resData={restaurant} />
